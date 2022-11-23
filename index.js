a5d4677c9e2f43ff5e9d490122d9b1327bb53266
Daniel Bacab <Daniel.Bacab@itksquare.edu.mx>
2022-11-23 15:13:39 -0600

    change on addEventListener to load

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 //Playing The Office's theme when the page loads
-window.addEventListener("DOMContentLoaded", event => {
+window.addEventListener("load", event => {
    const audio = document.querySelector("audio");
        audio.volume = 0.05;
        audio.loop = true;
