0556ee685d012517fa286be4025be2b0eee68a6a
Daniel Bacab <Daniel.Bacab@itksquare.edu.mx>
2022-11-23 16:07:29 -0600

    user story 1 solution 2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 //Playing The Office's theme when the page loads
-document.body.addEventListener("mousemove", event => {
+document.body.addEventListener("click", event => {
    const audio = document.querySelector("audio");
        audio.volume = 0.05;
        audio.loop = true;
