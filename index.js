18bd4a4aae0d2a4ca2aaa5f0d936500b0fc17cb6
Genaro Arredondo <Genaro.Arredondo@itksquare.edu.mx>
2022-11-24 15:34:04 -0600

    UserStory3

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 
 //Playing The Office's theme when the page loads
-window.addEventListener("DOMContentLoaded", event => {
-   const audio = document.querySelector("audio");
-       audio.volume = 0.05;
-       audio.loop = true;
-       audio.play();
-  });
-
-  
-
+document.body.addEventListener("click", event => {
+    const audio = document.querySelector("audio");
+        audio.volume = 0.05;
+        audio.loop = true;
+        audio.muted = false;
+        audio.play();
+   });
+ 
+   const banner = document.querySelector('.banner');
+ 
+   window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 0);
+})
+   
+ 
+ 
